4454896e5ae4ece041baeaf0e4f32887d7affa88
lohi1996@gmail.com <lohi1996@gmail.com>
2021-08-14 01:12:56 +0530

    header removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Footer from './Footer'
 import './App.css';
-import Header from './Header';
+
 
 function App() {
   return (
     <div className="App">
     ----Hemendra---  Welcome to HLKartDev -- Lohi
     <Footer />
-    <Header/>
+
     </div>
   );
 }
