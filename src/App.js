a4a85a386b10931ec5e722abd07eb44c0092a29d
lohi1996@gmail.com <lohi1996@gmail.com>
2021-08-14 00:51:10 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Footer from './Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     ----Hemendra---  Welcome to HLKartDev -- Lohitha
+    <Footer />
     </div>
   );
 }
