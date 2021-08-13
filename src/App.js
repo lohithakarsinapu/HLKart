b3332556aedbb98460544726b403c88b13e72d29
hemendrasundar <hemendrasundar@gmail.com>
2021-08-14 00:38:31 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import header from './header';
 
 function App() {
   return (
     <div className="App">
     ----Hemendra---  Welcome to HLKartDev -- Lohitha
+    <header></header>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class header extends Component {
+    render() {
+        return (
+            <div>
+                This header
+            </div>
+        )
+    }
+}
+
+export default header
